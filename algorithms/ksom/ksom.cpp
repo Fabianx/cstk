ee2422a3cff75ab5849fa60c4bd6db55ada8588d
hansiwurst <hansiwurst>
2004-10-28 16:43:00 +0000

    new ksom implementation with DVector

--- algorithms/ksom/ksom.cpp
+++ algorithms/ksom/ksom.cpp
@@ -18,131 +18,217 @@
 #include "ksom.h"
 
 #define _to3(x,y,l) (x)+((y)*max_x)+((l)*max_x*max_y)
+#define _to2(x,y) (((x)*max_y)+y)
 
-KSOM::KSOM(){
-    vect = NULL;
+KSOM::KSOM()
+{
+	//printf("KSOM()1\n");
+    	vect = NULL;
+	c=2; 
+	epoch=0;
+	nb_radius=5;
+	exp=2;
+    	win_xy = NULL;
+    	max_xy = NULL;
+	//printf("KSOM()2\n");
 }
 
-KSOM::~KSOM(){
-    if (vect!=NULL) delete []vect;
+KSOM::KSOM(vei_t x, vei_t y, vei_t n,ve_t distance, ve_t neighbourfct, bool autolearn, ve_t learnfct)
+{
+	c=2; 
+	epoch=0;
+	nb_radius=5;
+	exp=2;
+	win_xy = new KVector(2);
+	max_xy = new KVector(2);
+	max_xy->add_comp(x,0);
+	max_xy->add_comp(y,1);
+	max_y = y; max_x = x;
+	dist = distance;
+	nfct = neighbourfct;
+	autol = autolearn;
+	lfct = learnfct;
+	vect = new DVector[_to2(x-1,y)];
+	for (vei_t i=0; i<(_to2(x-1,y)); i++)
+		vect[i].create(n);
 }
 
-void KSOM::createGrid(vei_t x, vei_t y, vei_t l) {
-    vect = new ve_t[x*y*l];
-    max_x = x;
-    max_y = y;
-    max_l = l;
+KSOM::~KSOM()
+{
+    	if (vect!=NULL) delete []vect;
+    	delete win_xy;
+    	delete max_xy;
 }
 
-void KSOM::initRandom(ve_t base) {
-    vei_t x,y,l;    
-    for (x=0; x<max_x; x++) {
-      for (y=0; y<max_y; y++) {
-         for (l=0; l<max_l; l++) {
-              vect[_to3(x,y,l)] = (ve_t) (25.0*rand()/(RAND_MAX+1.0));
-         }           
-      }        
-    }
+void KSOM::create(vei_t x, vei_t y, vei_t n,ve_t distance, ve_t neighbourfct, bool autolearn, ve_t learnfct)
+{
+	//printf("create(..)1\n");
+	win_xy = new KVector(2);
+    	max_xy = new KVector(2);
+    	max_xy->add_comp(x,0);
+	max_xy->add_comp(y,1);
+	max_y = y; max_x = x;
+	dist = distance;
+	nfct = neighbourfct;
+	autol = autolearn;
+	lfct = learnfct;
+	vect = new DVector[_to2(x-1,y)];
+	for (vei_t i=0; i<(_to2(x-1,y)); i++)
+		vect[i].create(n);
 }
 
-void KSOM::feed_diam(KVector *kvect, float lr) {
-    ves_t dist, mindist = max_ves;
-    vei_t x,y,i;
-    float nb;    
-    // find winner coords
-    for (x=0; x<max_x; x++) {
-      for (y=0; y<max_y; y++) {
-         dist = 0;
-         for (i=0; i<max_l; i++)
-           dist += abs( kvect->pvect[i] - vect[_to3(x,y,i)] );
-         if (dist<mindist) {
-           mindist  = dist;
-           winner_x = x;
-           winner_y = y;            
-         }
-      }
-    }
-    // update
-    for (x=0; x<max_x; x++) {
-      for (y=0; y<max_y; y++) {
-         for (i=0; i<max_l; i++) {  
-           // linear decreasing diamond
-            nb = 1.0/(1.0+(abs(x-winner_x) + abs(y-winner_y)));
-           // mexican hat:
-            //nb = sqrt( (x-winner_x)*(x-winner_x) + (y-winner_y)*(y-winner_y) );
-            //nb = sin(nb)/(nb);
-            //if (nb<vect[_to3(x,y,i)]) nb=0;
-           // update:
-            vect[_to3(x,y,i)] += ( (ve_t)(lr*nb*(float)(kvect->pvect[i] - vect[_to3(x,y,i)])) );
-         }
-      }
-    }             
+void KSOM::initRandom(DVector& prototyp)
+{   
+for (vei_t x=0; x<max_xy->pvect[0]; x++) 
+{
+      	for (vei_t y=0; y<max_xy->pvect[1]; y++) 
+      	{
+         	for (vei_t n=0; n<prototyp.get_dim(); n++) 
+		{
+			switch (prototyp.get_type(n))
+			{
+				case U8B_TYPE:  vect[_to2(x,y)].set_comp(((u_8b_max)*((double)(rand())/(double)(RAND_MAX+1))),U8B_TYPE,n); 
+						break;
+				case U16B_TYPE: vect[_to2(x,y)].set_comp(((u_16b_max)*((double)(rand())/(double)(RAND_MAX+1))),U16B_TYPE,n); 
+						break;
+				case U32B_TYPE: vect[_to2(x,y)].set_comp(((u_32b_max)*((double)(rand())/(double)(RAND_MAX+1))),U32B_TYPE,n); 
+						break;
+				/*case U64B_TYPE: vect[_to2(x,y)].set_comp(((u_64b_max)*((double)(rand())/(double)(RAND_MAX+1))),U64B_TYPE,n); 
+						break;*/
+				case S8B_TYPE:  if (floor(2.0*rand()/(RAND_MAX+1.0))==1)
+							vect[_to2(x,y)].set_comp(((s_8b_max)*((double)(rand())/(double)(RAND_MAX+1))),  S8B_TYPE,n); 
+						else
+							vect[_to2(x,y)].set_comp(((s_8b_min)*((double)(rand())/(double)(RAND_MAX+1))),  S8B_TYPE,n); 
+						break;
+				case S16B_TYPE: if (floor(2.0*rand()/(RAND_MAX+1.0))==1)
+							vect[_to2(x,y)].set_comp(((s_16b_max)*((double)(rand())/(double)(RAND_MAX+1))),  S16B_TYPE,n); 
+						else
+							vect[_to2(x,y)].set_comp(((s_16b_min)*((double)(rand())/(double)(RAND_MAX+1))),  S16B_TYPE,n); 
+						break;
+				case S32B_TYPE: if (floor(2.0*rand()/(RAND_MAX+1.0))==1)
+							vect[_to2(x,y)].set_comp(((s_32b_max)*((double)(rand())/(double)(RAND_MAX+1))),  S32B_TYPE,n); 
+						else
+							vect[_to2(x,y)].set_comp(((s_32b_min)*((double)(rand())/(double)(RAND_MAX+1))),  S32B_TYPE,n); 
+						break;
+				/*case S64B_TYPE: if (floor(2.0*rand()/(RAND_MAX+1.0))==1)
+							vect[_to2(x,y)].set_comp(((s_64b_max)*((double)(rand())/(double)(RAND_MAX+1))),  S64B_TYPE,n); 
+						else
+							vect[_to2(x,y)].set_comp(((s_64b_min)*((double)(rand())/(double)(RAND_MAX+1))),  S64B_TYPE,n); 
+						break;*/
+				case F32B_TYPE: if (floor(2.0*rand()/(RAND_MAX+1.0))==1)
+							vect[_to2(x,y)].set_comp(((f_32b_max)*((double)(rand())/(double)(RAND_MAX+1))),  F32B_TYPE,n); 
+						else
+							vect[_to2(x,y)].set_comp(((f_32b_min)*((double)(rand())/(double)(RAND_MAX+1))),  F32B_TYPE,n);  
+						break;
+				case F64B_TYPE: if (floor(2.0*rand()/(RAND_MAX+1.0))==1)
+							vect[_to2(x,y)].set_comp(((f_64b_max)*((double)(rand())/(double)(RAND_MAX+1))),  F64B_TYPE,n); 
+						else
+							vect[_to2(x,y)].set_comp(((f_64b_min)*((double)(rand())/(double)(RAND_MAX+1))),  F64B_TYPE,n);  
+						break;
+			}
+        	}           
+	}        
+}
+/*printf("\n");
+for (vei_t x=0; x<max_xy->pvect[0]; x++) 
+{
+      	for (vei_t y=0; y<max_xy->pvect[1]; y++) 
+	{
+		printf("vec_%i_%i = vec[%i]",x,y,_to2(x,y));
+		for (vei_t n=0; n<prototyp.get_dim(); n++) 
+			printf("%7.3e ",vect[_to2(x,y)].get_comp(n));
+		printf("\n");
+	}
+}
+printf("\n");*/	
 }
 
-void KSOM::feed_circ(KVector *kvect, float lr) {
-
-    ves_t dist, mindist = max_ves;
-    vei_t x,y,i;
-    float nb;    
-    // find winner coords
-    for (x=0; x<max_x; x++) {
-      for (y=0; y<max_y; y++) {
-         dist = 0;
-         for (i=0; i<max_l; i++)
-           dist += abs( kvect->pvect[i] - vect[_to3(x,y,i)] );
-         if (dist<mindist) {
-           mindist  = dist;
-           winner_x = x;
-           winner_y = y;            
-         }
-      }
-    }
-    // update
-    for (x=0; x<max_x; x++) {
-      for (y=0; y<max_y; y++) {
-         for (i=0; i<max_l; i++) {  
-           // linear decreasing circle
-            nb = 1.0/(1.0+sqrt( (x-winner_x)*(x-winner_x) + (y-winner_y)*(y-winner_y) )); 
-           // update:
-            vect[_to3(x,y,i)] += ( (ve_t)(lr*nb*(float)(kvect->pvect[i] - vect[_to3(x,y,i)])) );
-         }
-      }
-    }             
+oas_t KSOM::det_dis(DVector& vec1, DVector& vec2)
+{
+	switch (dist)
+	{
+		case DIS_MANH:	return vec1.dis_manh(vec2);
+		case DIS_CHEB:	return vec1.dis_cheb(vec2);
+		case DIS_EUCL:	return vec1.dis_eucl(vec2);
+		case DIS_MINK:	return vec1.dis_mink(vec2, exp);
+	}
+	return vec1.dis_manh(vec1);
 }
 
-void KSOM::feed_bell(KVector *kvect, float lr, float nb_radius ) {
-    ves_t dist, mindist = max_ves;
-    vei_t x,y,i;
-    float nb;    
-    // find winner coords
-    for (x=0; x<max_x; x++) {
-      for (y=0; y<max_y; y++) {
-         dist = 0;
-         for (i=0; i<max_l; i++)
-           dist += abs( kvect->pvect[i] - vect[_to3(x,y,i)] );
-         if (dist<mindist) {
-           mindist  = dist;
-           winner_x = x;
-           winner_y = y;            
-         }
-      }
-    }
-    // update
-    for (x=0; x<max_x; x++) {
-      for (y=0; y<max_y; y++) {
-         for (i=0; i<max_l; i++) {  
-           // gaussian (bell shaped):
-            nb = sqrt( (x-winner_x)*(x-winner_x) + (y-winner_y)*(y-winner_y) );
-            nb = exp((-0.5*nb)/(2*nb_radius*nb_radius));                 
-            vect[_to3(x,y,i)] += ( (ve_t)(lr*nb*(float)(kvect->pvect[i] - vect[_to3(x,y,i)])) );
-         }
-      }
-    }             
+oas_t KSOM::det_dis(KVector& vec1, KVector& vec2)
+{
+	switch (dist)
+	{
+		case DIS_MANH:	return vec1.dis_manh(vec2);
+		case DIS_CHEB:	return vec1.dis_cheb(vec2);
+		case DIS_EUCL:	return vec1.dis_eucl(vec2);
+		case DIS_MINK:	return vec1.dis_mink(vec2, exp);
+	}
+	return vec1.dis_manh(vec1);
 }
 
-void KSOM::getCell(vei_t x, vei_t y, KVector *kvect) {
-    for (vei_t i=0; i<max_l; i++)
-       kvect->pvect[i] = vect[_to3(x,y,i)] ;
+void KSOM::feed(DVector& vec, float lr) 
+{
+	KVector xy(2);
+    	oas_t dist, mindist = (2.0 * DBL_MAX);;
+    	float nb;    
+	// find winner coords
+    	for (vei_t x=0; x<max_xy->pvect[0]; x++) 
+	{
+      		for (vei_t y=0; y<(max_xy->pvect[1]-1); y++) 
+      		{
+         		dist = det_dis(vect[_to2(x,y)],vec);
+         		if (dist<mindist) 
+			{
+           			mindist  = dist;
+           			win_xy->add_comp(x, 0);
+           			win_xy->add_comp(y, 1);       
+         		}
+      		}
+    	}
+	winner_x = win_xy->pvect[0];
+	winner_y = win_xy->pvect[1]; 
+    	// update the grid
+	for (vei_t x=0; x<max_xy->pvect[0]; x++) 
+	{
+      		for (vei_t y=0; y<max_xy->pvect[1]; y++) 
+      		{ 
+           		xy.add_comp(x, 0);
+			xy.add_comp(y, 1);
+	   		nb = det_nb(*win_xy, xy);
+			//printf("nb=%7.3e\n",nb);
+	    		if (autol)
+				vect[_to2(x,y)] +=(det_lr(lr) * nb * (vec - vect[_to2(x,y)]));
+			else
+            			vect[_to2(x,y)] +=(lr * nb * (vec - vect[_to2(x,y)]));
+         	}
+      	}         
+}
+
+oas_t KSOM::det_nb(KVector& vec1, KVector& vec2)
+{
+	switch (nfct)
+	{
+		case LINNB:	return (1.0/(1.0 + vec1.dis_eucl(vec2)));
+		case MANHNB:	return (1.0/(1.0 + vec1.dis_manh(vec2)));
+		case MEXNB:	return ((-0.5*vec1.dis_manh(vec2))/(2*nb_radius*nb_radius));
+		case GAUSSNB:   return 0.0;
+	}
+}
+
+oas_t KSOM::det_lr(oas_t lr)
+{
+	switch (lfct)
+	{
+		case LIN:	return (-((1/c) * epoch) + lr);
+		case LOG:	return (1/log(lr * epoch));
+		case EXP:	return (1/(lr*epoch));
+	}
+}
+
+oas_t KSOM::getCell(vei_t x, vei_t y, vei_t i) 
+{
+    return vect[_to2(x,y)].get_comp(i);
 }
 
 
--- algorithms/ksom/ksom.h
+++ algorithms/ksom/ksom.h
@@ -1,8 +1,8 @@
 /***************************************************************************
                               ksom.h  -  v.1.00
                              -------------------
-    begin                : Tue Nov 25 2003
-    copyright            : (C) 2003 by Kristof Van Laerhoven
+    begin                : Mon Oct 25 2004
+    copyright            : (C) 2004 by Martin Berchtold
     email                : 
  ***************************************************************************/
 
@@ -18,36 +18,64 @@
 #ifndef KSOM_H
 #define KSOM_H
 
-/**2D Kohonen Self-Organizing Map
-  *@author Kristof Van Laerhoven
-  
-  */
-
-#include "cstk_base/vector/kvector.h"   // for the types & basic vector
 #include <stdlib.h>  // rand
 #include <math.h>
 
+#include "cstk_base/vector/dvector.h"   // for the types & basic vector
+#include "cstk_base/vector/kvector.h"   // x/y winner and max
+
+#define DIS_MANH 0
+#define DIS_CHEB 1
+#define DIS_EUCL 2
+#define DIS_MINK 3
+
+#define LIN 0
+#define LOG 1
+#define EXP 2
+
+#define LINNB 0
+#define MANHNB 1
+#define MEXNB 2
+#define GAUSSNB 3
+
 class KSOM {
  public: 
-  KSOM();
-  ~KSOM();
-  /* create the map with the right dimensions */
-  void createGrid(vei_t x, vei_t y, vei_t l);
-  /* randomize the contents of the map's cells */
-  void initRandom(ve_t base);
-  /* feed a vector into the map, bnf = 1/(1+manhattan) */
-  void feed_diam(KVector *vect, float lr);
-  /* feed a vector into the map, bnf = 1/(1+euclidean) */
-  void feed_circ(KVector *vect, float lr);
-  /* feed a vector into the map, nbf = gaussian */
-  void feed_bell(KVector *vect, float lr, float nb_radius);
-  /* get the contents from one of the map's cells */
-  void getCell(vei_t x, vei_t y, KVector *kvect);
-  /* winner coords on the map*/
-  vei_t   winner_x, winner_y;    
+  	KSOM();
+  	/* create the map with the right dimensions and all parameters*/
+  	KSOM(vei_t x, vei_t y, vei_t n, ve_t distance=DIS_EUCL, ve_t neighbourfct=LINNB, bool autolearn=false, ve_t learnfct=LIN);
+  	~KSOM();
+  	/* create the map with the right dimensions and all parameters*/
+  	void create(vei_t x, vei_t y, vei_t n, ve_t distance=DIS_EUCL, ve_t neighbourfct=LINNB, bool autolearn=false, ve_t learnfct=LIN);
+  	/* randomize the contents of the map's cells, prototyp vector spezializes the range of each dimension*/
+  	void initRandom(DVector& prototyp);
+  	/* feed a vector into the map, lr specifies the learning rate when autolearn=false*/
+  	void feed(DVector& vec, float lr);
+  	/* get the value of the cell x/y*/
+  	oas_t getCell(vei_t x, vei_t y, vei_t i);
+	
+  	/* winner coords on the map*/
+  	KVector *win_xy;  
+	vei_t winner_x, winner_y; 
+	vei_t max_x, max_y; 
+	
+	oas_t c;
+	/*number of epochs trained jet*/ 
+	vei_t epoch;
+	/*neighbourhood radius*/
+	oas_t nb_radius;
+	
  private:
-  ve_t *vect;
-  vei_t  max_x, max_y, max_l;  
+  	oas_t det_dis(DVector& vec1, DVector& vec2);
+	oas_t det_dis(KVector& vec1, KVector& vec2);
+	oas_t det_nb(KVector& vec1, KVector& vec2);
+	oas_t det_lr(oas_t lr);
+  	DVector *vect;
+  	KVector  *max_xy;  
+  	ve_t nfct;
+  	ve_t dist;
+	ve_t exp;
+  	bool autol;
+  	ve_t lfct;
 };
 
 #endif
