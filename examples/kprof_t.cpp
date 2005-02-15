5df7c8a8df516d979d28deef80ae8f4bd2b80ddc
kristofvl <kristofvl>
2005-02-15 13:22:18 +0000

    complete makeover to the new file

--- examples/kprof_t.cpp
+++ examples/kprof_t.cpp
@@ -18,110 +18,39 @@
 #include "kprof/kprof.h"
 #include <stdlib.h> // exit
 
-int main(int ac, char **args) {
+int main(int ac, char **argv) {
 
-  KProf profile;
-  if (ac<2) {
-    printf("\n supply an XML Settings Document please\n\n");
-    exit(1);
-  } else {
-    printf("\nParsing:\n\r");
-  }
-  int res = profile.parse(args[1],SETTP_RTP);
-  if (res!=0) {
-       printf("Error: Parse error on line %i.\n\r",res);
-       exit(1);
-  };      
-    
-  printf("\n\rINPUT: ");
-  switch (profile.input_mode) {
-    case IMODE_RS232: printf("rs232 (poll='%s')", profile.is.rs232->poll_char); 
-                      printf("[port='%s', br=%i]", profile.is.rs232->device,
-                      profile.is.rs232->baudrate);      
-                      break;
-    case IMODE_FILE:  printf("datafile (file='%s')", profile.is.filename); 
-                      break; 
-    case IMODE_PIPE:  printf("pipe"); break;
-    case IMODE_UDP:   printf("udp (%i)", profile.is.port); break;
-    case IMODE_SIM:   printf("sim"); break;
-    case IMODE_INT:   printf("int"); break; 
-    default: printf("?");
-  }
-  printf("\n\r");
-  { // print out the input channels:
-	if (!profile.is.firstch()) {
-	printf("  IChannel List is empty!\n\r");
+	KProf profile;
+	if (ac<2) {
+	printf("\n supply an XML Settings Document please\n\n");
+	exit(1);
 	} else {
-		do {
-			printf("  - channel id(%i), ", profile.is.get_id()); 
-			printf("bits(%i), ", profile.is.get_bits());
-			printf("%ssigned", (profile.is.get_sign())?" ":"un");
-			printf("\n\r");
-		} while (profile.is.nextch());
+	printf("\nParsing:\n\r");
 	}
-  }
-  printf("\n\r");  
-  { // print out the input columns:
-	if (!profile.is.firstcol()) {
-	printf("  IColumn List is empty!\n\r ");
-	} else {
-		do {
-			printf("  - column id(%i) ", profile.is.get_col_id()); 
-			printf("channel(%i), ", profile.is.get_col_channel()); 
-			printf("bits(%i), ", profile.is.get_col_bits());
-			printf("%s", (profile.is.get_col_sign())?" ":"un");
-			printf("signed\n\r");
-		} while (profile.is.nextcol());
-	}
-  }  
-  printf("\n\rOUTPUT: ");
-  switch (profile.output_mode) {
-    case OMODE_RS232: printf("rs232"); break;
-    case OMODE_FILE:  printf("datafile (file='%s')", profile.os.filename); 
-                      break;
-    case OMODE_PIPE:  printf("pipe"); break;
-    case OMODE_UDP:   printf("udp (%s,%i)", profile.os.host, profile.os.port); 
-                      break;
-    default: printf("?");
-  }
-  printf("\n\r");  
-  { // print out the output channels:
-	if (!profile.os.firstch()) {
-	printf("  OChannel List is empty!\n\r ");
-	} else {
-		do {
-			printf("  - channel id(%i), ", profile.os.get_id()); 
-			printf("bits(%i), ", profile.os.get_bits());
-			printf("%ssigned", (profile.os.get_sign())?" ":"un");
-			printf(", src(%i), ", profile.os.get_src());
-			printf("type(%s)\n\r", otypes[profile.os.get_type()] );
-		} while (profile.os.nextch());
-	}  
-  }  
-  { // print out the plots:
-	printf("\n\rWINDOW: ");
-	printf("(%i,%i,%i,%i), border=%i", profile.win.xpos, profile.win.ypos,
-	       profile.win.width, profile.win.height, profile.win.border);
-	printf(", framespeed:%i, skip:%i\n\r", profile.win.framespersec,
-	       profile.win.skip);
-	if (!profile.win.firstplot()) {
-	printf("  Plot list is empty!\n\r ");
-	} else {
-		do {
-			printf("  - plot slot:%i, ", profile.win.get_id());
-			printf("%s, ", sptypes[profile.win.get_type()] ); 
-			printf("src:%i, res.:%i", profile.win.get_src(), 
-			       profile.win.get_res()); 
-			printf(", %sscaled,", 
-			       (profile.win.get_scale())?" ":"un" );
-			printf(" color:%i\n\r", profile.win.get_colour() );
-		} while (profile.win.nextplot());
-	}
-  }  
-  printf("\n\rTOOL PARAMETERS: \n\r");
   
-  printf("\n\rx:%i, y:%i, lr: %f, nbr: %f\n\r", profile.par.gridsize[0], 
-            profile.par.gridsize[1], profile.par.lr, profile.par.nbr);  
-    
+	char buff[0x10000];
+	KProf kp;
+	
+	FILE* fp = fopen(argv[1],"r");
+	if (fp==NULL) {
+		printf("Error opening file %s.\n\r", argv[1]);
+		return -1;
+	}
+	
+	kp.parse(fp);
+	
+	kp.export_dtd(buff);
+	printf("--- DTD: -------------------------------------\n%s\n",buff);
+	
+	kp.export_xsd(buff);
+	printf("--- XSD: -------------------------------------\n%s\n",buff);
+	
+	if (kp.error()) { 
+		kp.export_err(buff); 
+		printf("%s",buff); 
+		return(-1);
+	}
+	else
+		return 0;
 }
 
