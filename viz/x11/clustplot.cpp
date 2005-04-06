d0d2d4a61e17baed0c9d10d3ac68f2709405e815
kristofvl <kristofvl>
2005-04-06 14:43:42 +0000

    quick fix for clustplot

--- viz/x11/clustplot.cpp
+++ viz/x11/clustplot.cpp
@@ -54,32 +54,31 @@ void ClustPlot::barplot(int x, int y, int max_x, int max_y,
 void ClustPlot::lineplot(int x, int y, int max_x, int max_y, 
                        KVector *vector, int bgcolour)
 {
-	/*if (win_width_old != win_width) {
-		cell_width  = (win_width-4)/max_x;
+	if (win_width_old != window->win_width) {
+		cell_width  = (window->win_width-4)/max_x;
 		bar_width   = (cell_width-2)/vector->get_dim();
-		win_width_old = win_width;
+		win_width_old = window->win_width;
 	}
-	if (win_height_old != win_height) {
-		cell_height = (win_height-4)/max_y;
+	if (win_height_old != window->win_height) {
+		cell_height = (window->win_height-4)/max_y;
 		bar_max     = (cell_height-2);
-		win_height_old = win_height;
+		win_height_old = window->win_height;
 	}
-    
-	XSetForeground(display, gc, BlackPixel(display,screen_num));
-	XDrawRectangle(display, buffer, gc, 2+cell_width*x, 2+cell_height*y, 
+	
+	XSetForeground(window->display, window->gc, BlackPixel(window->display,window->screen_num));
+	XDrawRectangle(window->display, window->buffer, window->gc, 2+cell_width*x, 2+cell_height*y, 
 						cell_width, cell_height);
-	XSetForeground(display, gc, plot_colours[bgcolour].pixel);
-	XFillRectangle(display, buffer, gc, 2+cell_width*x+1, 2+cell_height*y+1, 
+	XSetForeground(window->display, window->gc, window->plot_colours[bgcolour].pixel);
+	XFillRectangle(window->display, window->buffer, window->gc, 2+cell_width*x+1, 2+cell_height*y+1, 
 						cell_width-2, cell_height-2);
 	
 	for (vei_t i=1; i<vector->get_dim(); i++) {
-		XSetForeground(display, gc, plot_colours[i].pixel);
-		XDrawLine( display, buffer, gc, 
+		XSetForeground(window->display, window->gc, window->plot_colours[i].pixel);
+		XDrawLine( window->display, window->buffer, window->gc, 
 			2+cell_width*x+1+(bar_width*i),
 			2+cell_height*y+1+bar_max - vector->pvect[i-1]*bar_max/255,
 			2+cell_width*x+1+(bar_width*i)+bar_width/2, 
 			2+cell_height*y+1+bar_max - vector->pvect[i]*bar_max/255);
 	}
-  */
 }
 
