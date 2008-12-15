b5bef9fe608c9afee1e8792308e9c7878b439603
Fabian Franz <franz@teco.edu>
2008-12-15 17:32:30 +0100

    - Moved implementation to cmeans.cpp. - Removed wrong whitespaces.

--- algorithms/cmeans/cmeans.cpp
+++ algorithms/cmeans/cmeans.cpp
@@ -1,1 +1,44 @@
 #include "cmeans.h"
+
+CMeans::CMeans(list<DVector>* data, vei_t cluster_n, CMeansOptions* algorithm_options) 
+{
+	input=data;
+	free_input=false;
+
+	if (input == NULL)
+	{
+		input=new list<DVector>;
+		free_input=true;
+	}
+
+	anzcluster=cluster_n;
+	clusterCenters.resize(cluster_n);
+
+	options = algorithm_options;
+	free_options=false;
+
+	if (!options)
+	{
+		options = new CMeansOptions();
+		free_options=true;
+
+		// Set sane defaults (matlab)
+		options->exp = CMEANS_DEFAULT_EXP;
+		options->maxIter = CMEANS_DEFAULT_MAXITER;
+		options->epsilon = CMEANS_DEFAULT_EPSILON;
+		options->displayInfo = CMEANS_DEFAULT_DISPLAY_INFO;
+	}
+}
+
+CMeans::~CMeans()
+{
+	if (free_input)
+		delete input;
+	if (free_options)
+		delete options;
+}
+
+void CMeans::readInputVector(DVector vec)
+{
+	input->push_back(vec);
+}
--- algorithms/cmeans/cmeans.h
+++ algorithms/cmeans/cmeans.h
@@ -10,8 +10,13 @@
 using namespace std;
 
 #include "cstk_base/types.h"
-#include "cstk_base/vector/dvector.h" 
+#include "cstk_base/vector/dvector.h"
 #include "cstk_base/matrix/dvectormatrix.h"
+
+#define CMEANS_DEFAULT_EXP		2.0
+#define CMEANS_DEFAULT_MAXITER		100
+#define CMEANS_DEFAULT_EPSILON		1e-5
+#define CMEANS_DEFAULT_DISPLAY_INFO	false
 
 class CMeansOptions
 {
@@ -25,45 +30,9 @@ class CMeansOptions
 class CMeans 
 {
 	public:
-		CMeans(list<DVector>* data, vei_t cluster_n, CMeansOptions* algorithm_options = NULL) 
-		{
-			input=data;
-
-			free_input=false;
-			if (input == NULL)
-			{
-				input=new list<DVector>;
-				free_input=true;
-			}
-
-			anzcluster=cluster_n;
-
-			clusterCenters.resize(cluster_n);
-
-			options = algorithm_options;
-
-			free_options=false;
-
-			if (!options)
-			{
-				options = new CMeansOptions();
-				free_options=true;
-
-				// Set sane defaults (matlab)
-				options->exp = (vei_t)2.0;
-				options->maxIter = 100;
-				options->epsilon = (vei_t)1e-5;
-				options->displayInfo = true;
-			}
-		}
+		CMeans(list<DVector>* data, vei_t cluster_n, CMeansOptions* algorithm_options = NULL);
 		
-		~CMeans()
-		{
-			if (free_input)
-				delete input;
-			if (free_options)
-				delete options;
-		}	
+		~CMeans();
 		
 		virtual void run()=0; /**< process the c-means algorithm */
 
@@ -75,10 +44,7 @@ class CMeans
 
 		// Online version
 
-		void readInputVector(DVector vec)
-		{
-			input->push_back(vec);
-		}
+		void readInputVector(DVector vec);
 
 	protected:
 		list<DVector>* input;
